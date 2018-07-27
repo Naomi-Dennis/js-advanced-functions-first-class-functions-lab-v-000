7b2f4078846958bc70d2f9953957fc50bfbaae81
Naomi Dennis <njdennis94@gmail.com>
2018-07-27 16:51:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const returnFirstTwoDrivers = (list) => {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 function createFareMultiplier(value){
-   return (value) => { return value ** 2}
+   return (multi) => { return value ** multi}
 }
 
 function fareDoubler(fare){
